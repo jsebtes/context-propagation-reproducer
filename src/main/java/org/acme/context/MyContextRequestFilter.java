2d70f456ab71310769e72f60c6d95c1ea9a8b966
Jean-Sébastien TESSIER <jean-sebastien.tessier@laposte.net>
2021-03-18 01:07:25 +0100

    Add response filter to clear context

--- src/main/java/org/acme/context/MyContextRequestFilter.java
+++ src/main/java/org/acme/context/MyContextRequestFilter.java
@@ -1,10 +1,13 @@
 package org.acme.context;
 
 import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.server.ServerRequestFilter;
+import org.jboss.resteasy.reactive.server.ServerResponseFilter;
 
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -13,10 +16,15 @@ public class MyContextRequestFilter {
     private static final Logger LOGGER = Logger.getLogger(MyContextRequestFilter.class);
 
     @ServerRequestFilter(preMatching = true)
-    public void filter(ContainerRequestContext requestContext, HttpServerRequest httpServerRequest) throws IOException {
+    public void filterResquestPreMatching(ContainerRequestContext requestContext, HttpServerRequest httpServerRequest) throws IOException {
         String myValue = UUID.randomUUID().toString();
         MyContext.setMyValue(myValue);
         httpServerRequest.headers().add("myValue", myValue);
+    }
+
+    @ServerResponseFilter
+    public void filterResponse(ContainerResponseContext responseContext, HttpServerResponse httpServerResponse) {
+        MyContext.clear();
     }
 
 }
